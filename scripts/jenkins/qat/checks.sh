3241e6dc050bbea331d2193bfeafac0b29c1748a
Rivera Gonzalez, Julio C <julio.c.rivera.gonzalez@intel.com>
2019-11-14 14:32:58 -0600

    Simplify jenkins-qat checks
    Signed-off-by: Rivera Gonzalez, Julio C <julio.c.rivera.gonzalez@intel.com>

--- scripts/jenkins/qat/checks.sh
+++ scripts/jenkins/qat/checks.sh
@@ -11,49 +11,30 @@ set -o xtrace
 set -o errexit
 
 sudo dmesg | grep -i qat
+sudo cat /proc/cmdline
 
-HPAGES=$(sudo cat /proc/cmdline | grep "hugepages=1024")
-if [ -z "$HPAGES" ]; then
- echo "ERROR: no hugepages cmdline flag set."
+if [ 0 -eq $(ls -1 /sys/class/iommu|wc -l) ]; then
+ echo "ERROR: no iommu con flags set."
  ERROR=1
 fi
 
-IOMMU=$(sudo cat /proc/cmdline | grep "intel_iommu=on iommu=pt")
-if [ -z "$IOMMU" ]; then
- echo "ERROR: no iommu cmdline flags set."
- ERROR=1
-fi
-
-QAT=$(sudo swupd bundle-list | grep -i qat)
-if [ -z "$QAT" ]; then
+if [ 0 -eq $(sudo swupd bundle-list |grep -i qat |wc -l) ]; then
  echo "ERROR: linux-qat-firmware bundle is not installed."
  ERROR=1
 fi
 
-DPDK=$(sudo swupd bundle-list | grep -i dpdk)
-if [ -z "$DPDK" ]; then
- echo "ERROR: dpdk bundle is not installed."
- ERROR=1
-fi
-
-VFIO=$(sudo lsmod | grep -i vfio_pci)
-if [ -z "$VFIO" ]; then
+if [ 0 -eq $(lsmod |grep -i vfio_pci| wc -l) ]; then
  echo "ERROR: vfio_pci kernel module is not enabled."
  ERROR=1
 fi
 
-PFS=$(for i in 0434 0435 37c8 6f54 19e2; do sudo lspci -D -d 8086:$i; done)
-if [ -z "$PFS" ]; then
- echo "ERROR: no qat physical functions were found."
- ERROR=1
-fi
-
-VFS=$(for i in 0442 0443 37c9 19e3; do sudo lspci -D -d 8086:$i; done)
-if [ -z "$VFS" ]; then
+if [ 0 -eq $(for i in 0442 0443 37c9 19e3; do lspci -D -d 8086:$i; done|wc -l) ]; then
  echo "ERROR: no qat virtual functions were found."
  ERROR=1
 fi
 
 if [ -n "$ERROR" ]; then
   exit 1
+else
+  echo "OK: all checks pass."
 fi
