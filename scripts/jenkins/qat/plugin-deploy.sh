ef1eeb2015c16e601eef552750b96c96a5565c0f
Rivera Gonzalez, Julio C <julio.c.rivera.gonzalez@intel.com>
2019-11-19 18:44:20 -0600

    Add organization and/or tag support for set-version script
    This commit adds capability to replace all hard-coded image tags/versions
    and organization names (registry).
    
    Signed-off-by: Rivera Gonzalez, Julio C <julio.c.rivera.gonzalez@intel.com>

--- scripts/jenkins/qat/plugin-deploy.sh
+++ scripts/jenkins/qat/plugin-deploy.sh
@@ -13,8 +13,9 @@ set -o errexit
 REPO_ROOT=${WORKSPACE:-$(realpath $(dirname $0)/../../..)}
 REG=${REG:-intel/}
 TAG=${TAG:-devel}
-sed -i "s#intel/intel-qat-plugin:devel#${REG}intel-qat-plugin:${TAG}#g" ${REPO_ROOT}/deployments/qat_plugin/qat_plugin.yaml
-sed -i "s#intel/crypto-perf:devel#${REG}crypto-perf:${TAG}#g" ${REPO_ROOT}/deployments/qat_dpdk_app/base/crypto-perf-dpdk-pod-requesting-qat.yaml
+ORG=${REG%/}
+
+${REPO_ROOT}/scripts/set-version.sh --tag ${TAG} --org ${ORG}
 kubectl create -f ${REPO_ROOT}/deployments/qat_plugin/qat_plugin_default_configmap.yaml
 kubectl create -f ${REPO_ROOT}/deployments/qat_plugin/qat_plugin.yaml
 kubectl rollout status ds/intel-qat-plugin --timeout=5m
--- scripts/set-version.sh
+++ scripts/set-version.sh
@@ -4,15 +4,53 @@
 #
 # SPDX-License-Identifier: Apache-2.0
 #
-# Invoke this script with a version as parameter
-# and it will update all hard-coded image versions
-# in the source code.
+# Invoke this script with a version/tag and/or an org
+# as parameter and it will update all hard-coded image
+# versions and/or orgs in the source code.
 #
 # Adapted from https://github.com/intel/pmem-csi/
 
-if [ $# != 1 ] || [ "$1" = "?" ] || [ "$1" = "--help" ]; then
-    echo "Usage: $0 <image version>" >&2
+OPTS=`getopt -o ht:o: --long help,tag:,org: -n 'test' -- "$@"`
+
+if [ $? != 0 ]; then
+    echo "Failed parsing options." >&2
     exit 1
 fi
 
-sed -i -e "s;\(^TAG?*=\|intel/crypto-perf:\|intel/opae-nlb-demo:\|intel/intel-[^ ]*:\)[^ ]*;\1$1;g" $(git grep -l '^TAG?*=\|intel/crypto-perf:\|intel/opae-nlb-demo:\|intel/intel-[^ ]*:' Makefile deployments demo/*fpga*.yaml)
+while true; do
+  case "${1}" in
+    -h | --help) usage="true"; shift ;;
+    -o | --org ) org=$2; shift 2;;
+    -t | --tag ) tag=$2; shift 2;;
+    -- ) shift; break ;;
+    * )  break ;;
+  esac
+done
+
+if [ "${usage}" ]; then
+  cat <<-EOF
+  Set custom versions and/or custom orgs
+
+  Usage:
+
+    set-version.sh [arguments]
+
+  Arguments:
+
+    -t or --tag     'Set a custom image version/tag'           Example: 'set-version.sh -t v1.1.0'
+    -o or --org     'Set a custom registry organization'       Example: 'set-version.sh -o docker.io'
+    -h or --help    'Show this'
+	EOF
+  exit 0;
+fi
+
+cd $(realpath $(dirname $0)/..)
+
+if [ "$tag" ]; then
+  sed -i -e "s;\(^TAG?*=\|.*/crypto-perf:\|.*/opae-nlb-demo:\|.*/intel-[^ ]*:\)[^ ]*;\1$tag;g" $(git grep -l '^TAG?*=\|.*/crypto-perf:\|.*/opae-nlb-demo:\|.*/intel-[^ ]*:' Makefile deployments demo/*fpga*.yaml)
+fi
+
+if [ "$org" ]; then
+  sed -i -e "s;\(^ORG?*=\)[^ ]*;\1$org;g" $(git grep -l '^ORG?*=\|.*/crypto-perf:\|.*/opae-nlb-demo:\|.*/intel-[^ ]*:' Makefile deployments demo/*fpga*.yaml)
+  sed -i -e "s;\(.*image:[ ]*\).*\(/\(intel\|crypto-perf\|opae-nlb-demo\)[^ ]*\);\1$org\2;g" $(git grep -l '^ORG?*=\|.*/crypto-perf:\|.*/opae-nlb-demo:\|.*/intel-[^ ]*:' Makefile deployments demo/*fpga*.yaml)
+fi
