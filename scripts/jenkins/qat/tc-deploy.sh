4f5fe5666ddb40ac1a78854ec85f23ed405e2863
Rivera Gonzalez, Julio C <julio.c.rivera.gonzalez@intel.com>
2019-11-25 19:54:55 -0600

    Deploys all test-cases at same time
    This commits change the loop to deploy test cases in parallel instead,
    of a sequence.
    
    Signed-off-by: Rivera Gonzalez, Julio C <julio.c.rivera.gonzalez@intel.com>

--- scripts/jenkins/qat/tc-deploy.sh
+++ scripts/jenkins/qat/tc-deploy.sh
@@ -12,9 +12,8 @@ set -o xtrace
 set -o errexit
 
 REPO_ROOT=$(realpath $(dirname $0)/../../..)
-for test in $(ls ${REPO_ROOT}/deployments/qat_dpdk_app/ | grep test-*); do
-  tname=$(echo $test |sed -e "s;\(test-[a-z]*\)\([0-9]*\);\1;g")
-  tnum=$(echo $test|sed -e "s;\(test-[a-z]*\)\([0-9]*\);\2;g")
-  kubectl apply -k ${REPO_ROOT}/deployments/qat_dpdk_app/${test}/ && \
-  kubectl wait --for=condition=Ready pod/qat-dpdk-${tname}-perf-tc${tnum} --timeout=5m
+for test in $(ls -d ${REPO_ROOT}/deployments/qat_dpdk_app/test-*); do
+  kubectl apply -k $test &
 done
+wait
+kubectl wait --for=condition=Ready pod --all --timeout=5m && sleep 60s
