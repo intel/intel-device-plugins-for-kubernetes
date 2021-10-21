7e292bb1d8b4518addac6b6fbab52c9cbf703e43
Ed Bartosh <eduard.bartosh@intel.com>
2021-10-21 11:53:53 +0300

    dlb: add dpdk demo
    Signed-off-by: Ed Bartosh <eduard.bartosh@intel.com>
    Signed-off-by: Hyeongju Johannes Lee <hyeongju.lee@intel.com>

---
+++ demo/dlb-dpdk-demo/test.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+dlb_dev=$(ls /dev/dlb* | sed 's/\/dev\/dlb//' | head -1)
+
+echo '\n1. Order queue test'
+echo '-------------------'
+dpdk-test-eventdev --no-huge --vdev="dlb2_event,dev_id=`echo $dlb_dev`" -- --test=order_queue --nb_flows 64 --nb_pkts 512  --plcores 1 --wlcores 2-7
+echo '\n2. Perf queue test'
+echo '------------------'
+dpdk-test-eventdev --no-huge --vdev="dlb2_event,dev_id=`echo $dlb_dev`" -- --test perf_queue  --nb_flows 64 --nb_pkts 1024 --plcores 1 --wlcores 2-7 --stlist o,a,a,o
+echo '\n3. Order atq test'
+echo '-----------------'
+dpdk-test-eventdev --no-huge --vdev="dlb2_event,dev_id=`echo $dlb_dev`" -- --test order_atq   --nb_flows 64 --nb_pkts 1024 --plcores 1 --wlcores 2-7
+echo '\n4. Perf atq test'
+echo '----------------'
+dpdk-test-eventdev --no-huge --vdev="dlb2_event,dev_id=`echo $dlb_dev`" -- --test perf_atq    --nb_flows 64 --nb_pkts 1024 --plcores 1 --wlcores 2-7 --stlist o,a,a,o
