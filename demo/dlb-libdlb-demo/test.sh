7f415f54a1b93a0c66996299d4b2c592c3a9501b
Ed Bartosh <eduard.bartosh@intel.com>
2022-06-16 23:11:08 +0300

    e2e-dlb: make tests working in Simics
    - used different pods for PF and VF workloads
    - increased waiting timeout to 200s
    - used 'epoll' waiting mode for test apps
    - decreased amount of packets from 128 to 8 for test apps
    - output pod logs
    - done small code optimisations
    
    Signed-off-by: Ed Bartosh <eduard.bartosh@intel.com>

--- demo/dlb-libdlb-demo/test.sh
+++ demo/dlb-libdlb-demo/test.sh
@@ -3,8 +3,9 @@
 dlb_dev=$(ls /dev/dlb* | sed 's/\/dev\/dlb//' | head -1)
 echo '\n1. Directed Traffic test'
 echo '--------------------------'
-/usr/local/bin/dir_traffic -n 128 -d $dlb_dev
+/usr/local/bin/dir_traffic -n 8 -d $dlb_dev -w epoll
+
 echo '\n2. Load Balanced Traffic test'
 echo '-------------------------------'
-/usr/local/bin/ldb_traffic -n 128 -d $dlb_dev
+/usr/local/bin/ldb_traffic -n 8 -d $dlb_dev -w epoll
 
