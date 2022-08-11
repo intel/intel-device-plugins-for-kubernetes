50b985dde5c1247428b281d52c233cdab493121f
Mikko Ylinen <mikko.ylinen@intel.com>
2022-08-11 08:24:01 +0300

    bump go mod version to 1.18 and move to k8s.io v1.25.0-rc.0
    the e2e framework in v1.25 also reguires us to move to gingko/v2
    so make that change too.
    
    Signed-off-by: Mikko Ylinen <mikko.ylinen@intel.com>

--- test/e2e/fpga/fpga.go
+++ test/e2e/fpga/fpga.go
@@ -23,7 +23,7 @@ import (
 	"time"
 
 	"github.com/intel/intel-device-plugins-for-kubernetes/test/e2e/utils"
-	"github.com/onsi/ginkgo"
+	"github.com/onsi/ginkgo/v2"
 	v1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/api/resource"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
