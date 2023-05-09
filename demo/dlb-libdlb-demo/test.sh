05620c427f5ec74dab5bd72fd9c993f736a72386
Mikko Ylinen <mikko.ylinen@intel.com>
2023-05-09 17:17:17 +0300

    e2e: fix DLB testing
    We have suffered from intermitted lockups on GNR targets. This
    masked results from e2e-dlb and a broken setup was merged.
    
    The right fix is to do maintenance on the GNR targets but until
    that happens, just move from epoll to interrupt based testing that
    is known to work in the current configuration.
    
    Signed-off-by: Mikko Ylinen <mikko.ylinen@intel.com>

--- demo/dlb-libdlb-demo/test.sh
+++ demo/dlb-libdlb-demo/test.sh
@@ -3,9 +3,9 @@
 dlb_dev=$(ls /dev/dlb* | sed 's/\/dev\/dlb//' | head -1)
 echo '\n1. Directed Traffic test'
 echo '--------------------------'
-/usr/local/bin/dir_traffic -n 8 -d $dlb_dev -w epoll
+/usr/local/bin/dir_traffic -n 8 -d $dlb_dev -w interrupt
 
 echo '\n2. Load Balanced Traffic test'
 echo '-------------------------------'
-/usr/local/bin/ldb_traffic -n 8 -d $dlb_dev -w epoll
+/usr/local/bin/ldb_traffic -n 8 -d $dlb_dev -w interrupt
 
