4a448f6cbc49c3a7acf816b9d583b0a4233fee11
Tuomas Katila <tuomas.katila@intel.com>
2023-06-02 13:59:02 +0300

    operator: update to 0.27.0 images
    Signed-off-by: Tuomas Katila <tuomas.katila@intel.com>

--- pkg/controllers/reconciler.go
+++ pkg/controllers/reconciler.go
@@ -37,7 +37,7 @@ import (
 
 var (
 	bKeeper         = &bookKeeper{}
-	ImageMinVersion = versionutil.MustParseSemantic("0.26.0")
+	ImageMinVersion = versionutil.MustParseSemantic("0.27.0")
 )
 
 func init() {
